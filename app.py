5e69b13b206096d89472cbbca4039cba9f3369af
jamikrut <jamikrut@student.wszib.edu.pl>
2023-06-11 10:35:50 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, abort
+import random
+from model.country import db, find_by_name
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+colors = {
+    1: "red",
+    2: "blue",
+    3: "green"
+}
+
+
+@app.route('/color')
+def random_color():  # lab example
+    return str(colors.get(random.randint(1, 3)))
+
+
+@app.route('/hello-world')
+def hello_world_html():
+    return render_template('welcome.html', message='Aplikacje Server side są super!')
+
+
+@app.route('/countries')
+def random_country():
+    country_index = random.randint(0, 246)
+    country = db[country_index]
+
+    return render_template('country.html', country=country)
+
+
+# path variable: <>typ:nazwa>
+@app.route('/countries/<name>')
+def country_by_name(name: str):
+    try:
+        found_country = find_by_name(name)
+    except ValueError as ex:
+        abort(404, ex)
+    return render_template('country.html', country=found_country)
+
+# podejemy inta - idzie do innego end pointa
+@app.route('/countries/<int:name>')
+def other_func(name: int):
+    return str(name)
+
+# podgląd mappingów
+print(app.url_map)
+
+if __name__ == '__main__':
+    app.run()
